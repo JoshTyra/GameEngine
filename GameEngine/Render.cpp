e2569c9c7d4c86a16bad0e2ac3816269f744b336
JT <godman0912@gmail.com>
2024-04-28 20:15:50 -0500

    remove near/far plane values from Render constructor used for testing

--- GameEngine/Render.cpp
+++ GameEngine/Render.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 Renderer::Renderer(int width, int height, GLFWwindow* window)
-    : screenWidth(width), screenHeight(height), projectionMatrix(glm::mat4(1.0f)), nearPlane(0.1f), farPlane(80.0f), window(window) {
+    : screenWidth(width), screenHeight(height), projectionMatrix(glm::mat4(1.0f)), window(window) {
     frameBufferManager = std::make_unique<FrameBufferManager>(window);
 
     // Create the main scene framebuffer
